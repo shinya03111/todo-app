3112943d6907f194ee2ab7ff723ac50ff9baa2e3
谷本新也 <shinya03111@gmail.com>
2022-12-16 06:12:51 +0900

    before_action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,11 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update]
+
   def index
     @boards = Board.all
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def new
@@ -22,11 +23,9 @@ class BoardsController < ApplicationController
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    @board = Board.find(params[:id])
     if @board.update(board_params)
       redirect_to root_path, notice: '更新できました'
     else
@@ -44,5 +43,9 @@ class BoardsController < ApplicationController
   private
   def board_params
     params.require(:board).permit(:title, :content)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 end
