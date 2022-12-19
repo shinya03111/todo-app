486142290e50fa2edde447999524873cda54374a
谷本新也 <shinya03111@gmail.com>
2022-12-20 06:11:08 +0900

    formを部分テンプレートに変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -15,7 +15,7 @@ class BoardsController < ApplicationController
   def create
     @board = Board.new(board_params)
     if @board.save
-      redirect_to board_path(@board), notice: '保存できたよ'
+      redirect_to root_path, notice: '保存できたよ'
     else
       flash.now[:error] = '保存に失敗しました'
       render :new
--- app/models/board.rb
+++ app/models/board.rb
@@ -11,5 +11,4 @@
 class Board < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-  belongs_to :user
 end
