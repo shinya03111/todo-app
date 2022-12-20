9fa80a9b5f8b4e90b2e4c289facdfc889ed03bf4
谷本新也 <shinya03111@gmail.com>
2022-12-21 06:22:54 +0900

    destroy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -37,7 +37,7 @@ class BoardsController < ApplicationController
   end
 
   def destroy
-    board = Board.find(params[:id])
+    board = current_user.boards.find(params[:id])
     board.destroy!
     redirect_to root_path, notice: '削除に成功しました'
   end
