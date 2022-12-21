a7821d7ef6d108b2d78c4581f77278a4af82fef0
谷本新也 <shinya03111@gmail.com>
2022-12-22 06:46:48 +0900

    seedを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+jon = User.create!(email: 'jon@example.com', password: 'password')
+emily = User.create!(email: 'emily@example.com', password: 'password')
+
 3.times do
-  Board.create(
+  jon.boards.create!(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
+
+3.times do
+  emily.boards.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
